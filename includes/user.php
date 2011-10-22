a35257b2335d68f398a98d60dfca50223a1a95a2
Mark Karavan <mark.karavan@gmail.com>
2011-10-22 15:20:23 -0400

    Added user instantiation methods

--- includes/user.php
+++ includes/user.php
@@ -5,24 +5,59 @@
 require_once('database.php');
 
 class User {
-	
+
+	public $id;
+	public $username;
+	public $password;
+	public $first_name;
+	public $last_name;	
+
 	public static function find_all() {
-		global $database;
-		$result_set = self::find_by_sql("SELECT * FROM Users");
-		return $result_set;
+		return self::find_by_sql("SELECT * FROM Users");
 	}
 
 	public static function find_by_id($id=0) {
 		global $database;
-		$result_set = self::find_by_sql("SELECT * FROM users WHERE id = {$id} LIMIT 1");
-		$found = $database->fetch_array($result_set);
-		return $found;
+		$result_array = self::find_by_sql("SELECT * FROM users WHERE id = {$id} LIMIT 1");
+		return !empty($result_array) ? array_shift($result_array) : false;
 	}
 
 	public static function find_by_sql($sql="") {
 		global $database;
 		$result_set = $database->query($sql);
-		return $result_set;
+		$object_array = array();
+		while ($row = $database->fetch_array($result_set)) {
+			$object_array[] = self::instantiate($row);
+		}
+		return $object_array;
+	}
+	
+	public function full_name() {
+		if(isset($this->first_name) && isset($this->last_name)) {
+			return $this->first_name . " " . $this->last_name;
+		} else {
+			return "";
+		}
+	}
+	
+	private static function instantiate($record) {
+		$object = new self;
+		foreach($record as $attribute=>$value) {
+			if(self::has_attribute($attribute)) {
+				$object->$attribute = $value;
+			} 		
+		}
+		return $object;
+	}
+
+	private static function has_attribute($attribute) {
+		$object = new self;
+		//get_object_vars returns an associative array with all attributes
+		//(incl. private ones) as the keys and their current values as the value
+		$object_vars = get_object_vars($object);
+		//We don't care about the value, we just want to know if the key exists
+		//will return true or false
+		return array_key_exists($attribute, $object_vars);
 	}
 }
 
--- public/index.php
+++ public/index.php
@@ -5,14 +5,4 @@ require_once('../includes/user.php');
 
 $user = User::find_by_id(1);
 print_r($user);
-echo "<br/>";
-
-$user = User::find_by_sql("SELECT * FROM users WHERE first_name = 'marcus'");
-print_r($user);
-echo "<br/>";
-
-$user = User::find_all();
-print_r($user);
-echo "<br/>";
- 
 ?>
