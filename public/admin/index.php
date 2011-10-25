6eafa89a5bddf75994a3c383fa30bbb600e623e6
Mark Karavan <mark.karavan@gmail.com>
2011-10-25 01:28:49 -0400

    Made edits to logfile challenge

--- public/admin/index.php
+++ public/admin/index.php
@@ -7,7 +7,10 @@ if (!$session->is_logged_in()) { redirect_to("login.php"); }
 <?php include_layout_template('admin_header.php'); ?>
 
 		<h2>Menu</h2>
-		
+		<ul>
+			<li><a href="logfile.php">View Log File</a></li>
+			<li><a href="logout.php">Logout</a></li>
+		</ul>	
 		</div>
 		
 <?php include_layout_template('admin_footer.php'); ?>
--- public/admin/logfile.php
+++ public/admin/logfile.php
@@ -7,11 +7,14 @@ if (!$session->is_logged_in()) { redirect_to("login.php"); }
 <?php include_layout_template('admin_header.php'); ?>
 <?php if($_GET['clear']=='true') {$session->clear_logs();} ?>
 
-		<h2>Menu</h2>
+<a href="index.php">Back</a>
+
+		<h2>Log File</h2>
+
+<a href="logfile.php?clear=true">Clear Log Files</a>	
 
 <?php echo $session->read_log_entries(); ?>
 
-<a href="logfile.php?clear=true">Clear Log Files</a>	
 		</div>
 		
 <?php include_layout_template('admin_footer.php'); ?>
